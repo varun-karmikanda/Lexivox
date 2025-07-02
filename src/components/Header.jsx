d9c267a5914514ab1c71e2b92b88e4b69ab55205
varun-karmikanda <varunbkarmikanda@gmail.com>
2025-07-02 17:06:02 +0000

    improved stylings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between gap-4 p-4">
       <h1 className="font-medium text-xl">
-        Lexi<span className="text-pink-300 bold">Vox</span>
+        Lexi<span className="text-pink-300 bold animate-color-fade">Vox</span>
       </h1>
-      <button className="flex items-center gap-2">
+      <button className="flex items-center gap-2 text-pink-400 pinkShadow hover-animate-color-fade px-2 py-1.5">
         <p>New</p>
         <i className="fa-solid fa-plus"></i>
       </button>
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,19 +4,19 @@ export default function () {
   return (
     <main className="flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 justify-center text-center pb-30">
       <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
-        Lexi<span className="text-pink-300 bold">Vox</span>
+        Lexi<span className="text-pink-300 bold animate-color-fade">Vox</span>
       </h1>
       <h3 className="font-medium md:text-lg">
         Record{" "}
         <span className="text-pink-400">&rarr;</span> Transcribe{" "}
         <span className="text-pink-400">&rarr;</span> Translate
       </h3>
-      <button className="flex items-center justify-between text-base mx-auto w-72 max-w-full my-4">
-        <p>Record</p>
+      <button className="flex items-center justify-between text-base mx-auto w-72 max-w-full my-4 pinkShadow px-3 py-1.5">
+        <p className="text-pink-400">Record</p>
         <i className="fa-solid fa-microphone"></i>
       </button>
-      <p className="text-base md:text-lg">Or just <label className="text-pink-300 cursor-pointer hover:text-pink-600 duration-200 font-semibold">upload<input className="hidden" type="file" accept=".mp3,.wave"/></label> .mp3</p>
-      <p className="italic text-pink-400">Forever-free, because duh <span className="font-bold">&lt;3</span></p>
+      <p className="text-base md:text-lg">Or just <label className="text-pink-300 cursor-pointer hover:text-pink-600 duration-200 font-semibold hover-animate-color-fade">upload<input className="hidden" type="file" accept=".mp3,.wave"/></label> .mp3</p>
+      <p className="italic text-pink-400 animate-color-fade text-md">Forever-free, because duh <span className="font-bold">&lt;3</span></p>
     </main>
   );
 }
