29107a6940579803d97fbbf188da8f68c14bb447
varun-karmikanda <varunbkarmikanda@gmail.com>
2025-07-04 08:15:55 +0000

    Add clipboard copy and file download functionality

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -8,6 +8,19 @@ export default function Information(props) {
 
   const [tab,setTab] = useState('transcription');
 
+  function handleCopy(){
+    navigator.clipboard.writeText();
+  }
+
+  function handleDownload(){
+    const element = document.createElement('a');
+    const file = new Blob([],{ type:'text/plain' });
+    element.href = URL.createObjectURL(file);
+    element.download(`Lexivox_${(new Date()).toDateString()}.txt`);
+    document.body.appendChild(element);
+    element.click();
+  }
+
   return (
     <main className="flex-1 p-4 flex flex-col gap-1 sm:gap-2 justify-center text-center pb-30 max-w-prose w-full mx-auto">
       <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl pb-3">
@@ -25,11 +38,21 @@ export default function Information(props) {
           Translation
         </button>
       </div>
-      {tab === 'transcription' ? (
-        <Transcription {...props}/>
-      ) : (
-        <Translation {...props}/>
-      )}
+      <div className='flex flex-col my-8'>
+        {tab === 'transcription' ? (
+          <Transcription {...props}/>
+        ) : (
+          <Translation {...props}/>
+        )}
+      </div>
+      <div className='flex items-center gap-4 mx-auto'>
+        <button title='copy' className='p-2.5 text-pink-300 pinkShadow hover-animate-color-fade aspect-square grid place-items-center'>
+          <i className="fa-solid fa-copy"></i>
+        </button>
+        <button title='download' className='p-2.5 text-pink-300 pinkShadow hover-animate-color-fade aspect-square grid place-items-center'>
+          <i className="fa-solid fa-download"></i>
+        </button>
+      </div>
     </main>
   )
 }
